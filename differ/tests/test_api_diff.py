4551cdef3daeddac7d714e90ce1d54baca975240
Jakub Svoboda <jakub.svoboda@firma.seznam.cz>
2024-07-29 14:44:11 +0200

    JSON format validity check

--- differ/tests/test_api_diff.py
+++ differ/tests/test_api_diff.py
@@ -12,3 +12,13 @@ class TestApiDiff(BaseAPITestCase):
         }
         res = self.client.post("/api-admin/v1/diff/", data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_diff_validity(self):
+        data = {
+            "left_text": '{"abc": 1}',
+            "right_text": '"word"word'
+        }
+        res = self.client.post("/api-admin/v1/diff/", data, "json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["validity"]["left"], True)
+        self.assertEqual(res.data["validity"]["right"], False)
--- differ/views/admin.py
+++ differ/views/admin.py
@@ -2,6 +2,8 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from differ.JsonValidator import is_valid_json
+
 
 class DiffView(APIView):
     def post(self, request, *args, **kwargs):
@@ -11,8 +13,13 @@ class DiffView(APIView):
         right_text = data.get("right_text", "")
 
         response_data = {
+            "validity": {
+                "left": is_valid_json(left_text),
+                "right": is_valid_json(right_text)
+            },
             "left_text": left_text,
             "right_text": right_text
         }
+
 
         return Response(response_data, status=status.HTTP_200_OK)
